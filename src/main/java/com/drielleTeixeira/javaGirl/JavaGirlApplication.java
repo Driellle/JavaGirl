0863f17c2fb61dfb33853a82a7418e19ce823df2
ALEXSANDRO FERNANDES SARTORI ROLIANO <ALU0810510@uniritter.local>
2019-05-18 15:31:15 -0300

    primeiro commit

---
+++ src/main/java/com/drielleTeixeira/javaGirl/JavaGirlApplication.java
@@ -0,0 +1,13 @@
+package com.drielleTeixeira.javaGirl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGirlApplication.class, args);
+	}
+
+}
